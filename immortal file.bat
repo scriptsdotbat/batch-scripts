fe6297d8c330d1dcf83728a45fff9c2c42406b86
scriptsdotbat <110199700+scriptsdotbat@users.noreply.github.com>
2022-07-28 23:47:16 +0200

    Update immortal file.bat

--- immortal file.bat
+++ immortal file.bat
@@ -1,4 +1,21 @@
 @echo off
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Are you sure you want to continue? This will create an undeletable file.
+echo.
+echo Press any key to continue or close this window to cancel
+echo.
+echo Created by https://www.tiktok.com/@scripts.bat. Showcase video: https://www.tiktok.com/@scripts.bat/video/7124390807162850566
+echo.
+echo For educational purpose only! Do not use without anyones permission!
+pause>>nul
+cls
 cd %onedrive%\desktop
 cd %userprofile%\desktop
 echo.>>"Delete me"
