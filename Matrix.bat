31c511e2ce70f7c60370d44be230cc6fad3efc5c
scriptsdotbat <110199700+scriptsdotbat@users.noreply.github.com>
2022-07-28 23:44:35 +0200

    Update Matrix.bat

--- Matrix.bat
+++ Matrix.bat
@@ -1,4 +1,16 @@
 @echo off
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Created by https://www.tiktok.com/@scripts.bat. Showcase video: https://www.tiktok.com/@scripts.bat/video/7124390669459721477
+pause>>nul
+cls
+
 color 0a
 :loop
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
